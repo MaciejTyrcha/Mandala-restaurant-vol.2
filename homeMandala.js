2a752baf4d3da82608d4e97f300dadcd4a2cf758
aertuss <maciej.tyrcha@o2.pl>
2019-01-22 14:39:05 +0100

    fixed iframe map in contact section

---
+++ homeMandala.js
@@ -0,0 +1,13 @@
+$('.hamburgerMenu').click(function () {
+    $('nav').toggleClass('open', 500);
+    $('.hamburger').toggleClass('show');
+    $('.rotatedHamburger').toggleClass('show');
+})
+
+// const burger = document.querySelector('.hamburgerMenu');
+// const iconBurger = document.querySelector('.hamburger');
+// const iconX = document.querySelector('.fas.fa-times');
+
+// burger.addEventListener("click", function () {
+
+// })

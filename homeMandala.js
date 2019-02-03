981ba32b2e9f9e0dc5d9024fb9aab9c3ca7221e1
aertuss <maciej.tyrcha@o2.pl>
2019-02-03 22:34:20 +0100

    scroll first image home class

--- homeMandala.js
+++ homeMandala.js
@@ -6,7 +6,7 @@ $('.hamburgerMenu').click(function () {
 
 class OnLoad {
   constructor() {
-    this.divHomeFirstImage = "";
+    this.divHomeFirstImage = ""
   }
 
   showFirstImageSection() {
@@ -16,3 +16,30 @@ class OnLoad {
 }
 
 const loadHome = new OnLoad();
+
+class ScrollDivInImage {
+  constructor() {
+    this.windowScroll = 0
+    this.nextSection = ""
+    this.divContainer = ""
+    this.innerHeight = 0
+    this.innerWidth = 0
+  }
+
+  scroll() {
+    this.innerHeight = window.innerHeight;
+    this.innerWidth = window.innerWidth;
+    this.windowScroll = window.scrollY;
+    this.nextSection = document.querySelector(".top-section").offsetTop;
+    this.divContainer = document.querySelector(".first-image div section");
+
+    if (this.windowScroll < this.nextSection / 2.15 && this.innerWidth > 1100) {
+      this.divContainer.style.transform = `translate(0%, ${this.windowScroll / 3}%)`;
+    }
+    else if (this.windowScroll < this.nextSection / 3.5 + 25 && this.innerWidth <= 1100) {
+      this.divContainer.style.transform = `translate(0%, ${this.windowScroll / 3}%)`;
+    }
+  }
+}
+
+const scrollDivInImage = new ScrollDivInImage();

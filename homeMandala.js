bfa27e2dfff04e40a449acdce2ac8334285c5f53
aertuss <maciej.tyrcha@o2.pl>
2019-02-05 21:23:10 +0100

    second image home section class

--- homeMandala.js
+++ homeMandala.js
@@ -9,12 +9,17 @@ class MandalaHome {
     window.addEventListener("load", this.loadElement);
     window.addEventListener("scroll", this.scrollWeb);
   }
+
   loadElement() {
     this.load = new OnLoadHome();
-    this.load.showFirstImageSection();
+    this.load.showFirstImageHome();
   }
 
   scrollWeb() {
+
+    this.scrollSecondImageHome = new ScrollSecondImageHome();
+    this.scrollSecondImageHome.scrollSecondImageHome();
+    
     this.scrollTopSectionHome = new ScrollTopSectionHome();
     this.scrollTopSectionHome.scrollTopSection();
   }
@@ -27,20 +32,20 @@ class OnLoadHome {
     this.divHomeFirstImage = ""
   }
 
-  showFirstImageSection() {
+  showFirstImageHome() {
     this.divHomeFirstImage = document.querySelector(".first-image div section");
     this.divHomeFirstImage.classList.add("active");
   }
 }
 
 
-// class ScrollMandalaHome {
-//   constructor() {
-//     this.scrollValue = 0
-//     this.windowHeight = 0
-//     this.windowWidth = 0
-//   }
-// }
+class ScrollMandalaHome {
+  constructor() {
+    this.scrollValue = 0
+    this.windowHeight = 0
+    this.windowWidth = 0
+  }
+}
 
 
 
@@ -72,10 +77,10 @@ class ScrollTopSectionHome {
 
     for ( let i = 0; i < this.wholeContent.length; i++) {
       let wholeContent = this.wholeContent[i];
-      console.log(wholeContent);
+      // console.log(wholeContent);
       for ( let j = 0; j < wholeContent.length; j++) {
         let wholeContentDeeper = wholeContent[j];
-        console.log(wholeContentDeeper);
+        // console.log(wholeContentDeeper);
         if ( this.scrollValue > wholeContentDeeper.offsetTop + wholeContentDeeper.clientHeight - this.windowHeight) {
           wholeContentDeeper.classList.add('active');
         }
@@ -87,17 +92,30 @@ class ScrollTopSectionHome {
 
 class ScrollSecondImageHome{
   constructor() {
-    this.scrollValue = 0
+    this.divHomeSecondImage = ""
+    this.homeSecondImage = ""
     this.windowHeight = 0
-    this.windowWidth = 0
+    this.scrollValue = 0
+    }
+
+    scrollSecondImageHome() {
+      this.windowHeight = window.innerHeight;
+      this.scrollValue = window.scrollY;
+      this.homeSecondImage = document.querySelector(".second-image");
+      this.divHomeSecondImage = document.querySelector(".second-image div");
+      console.log(`Scroll value: ${this.scrollValue}, offsetTop: ${this.divHomeSecondImage.offsetTop}, clientHeight: ${this.divHomeSecondImage.clientHeight}, windowHeight: ${this.windowHeight}`);
+
+      if ( this.scrollValue > (this.divHomeSecondImage.offsetTop + this.homeSecondImage.offsetTop) - this.windowHeight) {
+        this.divHomeSecondImage.classList.add('active');
+      }
   }
 }
 
-class ScrollFooter {
-  constructor() {
-    this.scrollValue = 0
-    this.windowHeight = 0
-    this.windowWidth = 0
-  }
-}
+// class ScrollFooter {
+//   constructor() {
+//     this.scrollValue = 0
+//     this.windowHeight = 0
+//     this.windowWidth = 0
+//   }
+// }
 

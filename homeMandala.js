3db7fea8ae6f2732443bcd64ccfc6f0ab0c6c4b7
aertuss <maciej.tyrcha@o2.pl>
2019-02-05 16:55:13 +0100

    MandalaHome class added

--- homeMandala.js
+++ homeMandala.js
@@ -4,6 +4,18 @@ $('.hamburgerMenu').click(function () {
     $('.hamburgerWrapper').toggleClass('hamburgerWrapperOpen');
 })
 
+class MandalaHome {
+  constructor() {
+    window.addEventListener("load", this.loadElement);
+  }
+  loadElement() {
+    this.load = new OnLoad();
+    this.load.showFirstImageSection();
+  }
+}
+
+const mandalaHome = new MandalaHome();
+
 class OnLoad {
   constructor() {
     this.divHomeFirstImage = ""
@@ -17,37 +29,38 @@ class OnLoad {
 
 const loadHome = new OnLoad();
 
-class ScrollDivInImage {
-  constructor() {
-    this.windowScroll = 0
-    this.nextSection = ""
-    this.divContainer = ""
-    this.innerHeight = 0
-    this.innerWidth = 0
-  }
+// class ScrollDivInImage {
+//   constructor() {
+//     this.windowScroll = 0
+//     this.nextSection = ""
+//     this.divContainer = ""
+//     this.innerHeight = 0
+//     this.innerWidth = 0
+//   }
 
-  scroll() {
-    this.innerHeight = window.innerHeight;
-    this.innerWidth = window.innerWidth;
-    this.windowScroll = window.scrollY;
-    this.nextSection = document.querySelector(".top-section").offsetTop;
-    this.divContainer = document.querySelector(".first-image div section");
+//   scroll() {
+//     this.innerHeight = window.innerHeight;
+//     this.innerWidth = window.innerWidth;
+//     this.windowScroll = window.scrollY;
+//     this.nextSection = document.querySelector(".top-section").offsetTop;
+//     this.divContainer = document.querySelector(".first-image div section");
 
-    if (this.windowScroll < this.nextSection / 2.15 && this.innerWidth > 1100) {
-      this.divContainer.style.transform = `translate(0%, ${this.windowScroll / 3}%)`;
-    }
-    else if (this.windowScroll < this.nextSection / 3.5 + 25 && this.innerWidth <= 1100) {
-      this.divContainer.style.transform = `translate(0%, ${this.windowScroll / 3}%)`;
-    }
-  }
-}
+//     if (this.windowScroll < this.nextSection / 2.15 && this.innerWidth > 1100) {
+//       this.divContainer.style.transform = `translate(0%, ${this.windowScroll / 3}%)`;
+//     }
+//     else if (this.windowScroll < this.nextSection / 3.5 + 25 && this.innerWidth <= 1100) {
+//       this.divContainer.style.transform = `translate(0%, ${this.windowScroll / 3}%)`;
+//     }
+//   }
+// }
 
-const scrollDivInImage = new ScrollDivInImage();
+// const scrollDivInImage = new ScrollDivInImage();
 
 class ScrollTopSection {
   constructor() {
     this.scrollValue = 0
     this.windowHeight = 0
+    this.windowWidth = 0
     this.allImages = ""
     this.allP1 = ""
     this.allH1 = ""
@@ -59,6 +72,7 @@ class ScrollTopSection {
   scrollTopSection() {
     this.scrollValue = window.scrollY;
     this.windowHeight = window.innerHeight;
+    this.windowWidth = window.innerWidth;
 
     this.allImages = [...document.querySelectorAll(".top-section-image")];
     this.allP1 = [...document.querySelectorAll(".top-section-content p:nth-of-type(1)")];
@@ -70,8 +84,10 @@ class ScrollTopSection {
 
     for ( let i = 0; i < this.wholeContent.length; i++) {
       let wholeContent = this.wholeContent[i];
+      console.log(wholeContent);
       for ( let j = 0; j < wholeContent.length; j++) {
         let wholeContentDeeper = wholeContent[j];
+        console.log(wholeContentDeeper);
         if ( this.scrollValue > wholeContentDeeper.offsetTop + wholeContentDeeper.clientHeight - this.windowHeight) {
           wholeContentDeeper.classList.add('active');
         }

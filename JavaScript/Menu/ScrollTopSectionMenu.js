ef7bcd48068ac491f4d2cfce96ed7f06b42db2df
aertuss <maciej.tyrcha@o2.pl>
2019-02-12 21:38:52 +0100

    menu fixed all td's to show up in same time

--- JavaScript/Menu/ScrollTopSectionMenu.js
+++ JavaScript/Menu/ScrollTopSectionMenu.js
@@ -3,10 +3,12 @@ class ScrollTopSectionMenu {
     this.scrollValue = 0
     this.windowHeight = 0
     this.windowWidth = 0
+    this.topSection = ""
     this.allP = ""
     this.allH1 = ""
     this.allH2 = ""
-    this.allTd = ""
+    this.allTdFirstChild = ""
+    this.allTdAfter = ""
     this.wholeContent = []
   }
 
@@ -15,12 +17,14 @@ class ScrollTopSectionMenu {
     this.windowHeight = window.innerHeight;
     this.windowWidth = window.innerWidth;
 
+    this.topSection = document.querySelector(".top-section");
     this.allP = [...document.querySelectorAll(".top-section p")];
     this.allH1 = [...document.querySelectorAll(".top-section h1")];
     this.allH2 = [...document.querySelectorAll(".top-section h2")];
-    this.allTd = [...document.querySelectorAll(".top-section td")];
+    this.allTdFirstChild = [...document.querySelectorAll(".top-section td:first-child")];
+    this.allTdAfter = [...document.querySelectorAll(".top-section td:last-child")];
 
-    this.wholeContent = [this.allH1, this.allP, this.allH2, this.allTd];
+    this.wholeContent = [this.allH1, this.allP, this.allH2];
 
     for ( let i = 0; i < this.wholeContent.length; i++) {
       let wholeContent = this.wholeContent[i];
@@ -30,6 +34,15 @@ class ScrollTopSectionMenu {
           wholeContentDeeper.classList.add('active');
         }
       }
+    }
+    let k = 0;
+    for ( let j = 0; j < this.allTdFirstChild.length; j++) {
+      let allTdFirstChild = this.allTdFirstChild[j];
+      if ( this.scrollValue > allTdFirstChild.offsetTop + allTdFirstChild.clientHeight - this.windowHeight) {
+        allTdFirstChild.classList.add('active');
+        this.allTdAfter[k].classList.add('active');
+      }
+      k++;
     }
   }
 }
